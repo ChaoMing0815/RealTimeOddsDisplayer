8c5ab9c1be76a8f2fa1e2dc1d9c3b5364f5d1d0e
Chao Ming Huang <mingqed@gmail.com>
2025-08-07 09:52:02 +0800

    Initial Commit

---
+++ RealTimeOddsDisplayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealTimeOddsDisplayer
+//
+//  Created by 黃昭銘 on 2025/8/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
